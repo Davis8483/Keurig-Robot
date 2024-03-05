5d797780bb630ba1ffac0a241df50cd341622715
Noah Davis <davisprints3d@gmail.com>
2024-03-04 19:12:57 -0600

    stripe testing complete

--- stripe_test.py
+++ stripe_test.py
@@ -1,25 +1,33 @@
-
 import stripe
+import qrcode
 import time
 
 stripe.api_key = "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"
 
 product = stripe.Product.list().data[0]
-print(product)
 
-checkout = stripe.checkout.Session.create(
-  success_url="https://example.com/success",
-  line_items=[{"quantity": 1, "price": f"{product.default_price}"}],
-  mode="payment"
+# checkout = stripe.checkout.Session.create(
+#   success_url="https://stripe.com",
+#   line_items=[{"quantity": 1, "price": f"{product.default_price}"}],
+#   mode="payment"
+# )
+
+link = stripe.PaymentLink.create(
+    restrictions={"completed_sessions": {"limit": 1}},
+    line_items=[{"price": f"{product.default_price}", "quantity": 1}]
 )
 
-print(checkout.url)
+print(link.url)
+qr = qrcode.make(link.url)
+qr.show()
 
 while True:
-    # update checkout session from api
-    checkout = stripe.checkout.Session.retrieve(id=checkout.id)
+    
+    # retrieve the last 10 events
+    for event in stripe.Event.list(limit=10):
 
-    # check status of the checkout
-    print(checkout.payment_status)
+      # check if event originates from the payment link
+      if event.type=="checkout.session.completed" and event.data.object.payment_link==link.id:
+        print("payment successful")
     
     time.sleep(1)
