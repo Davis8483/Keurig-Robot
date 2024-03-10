6b371cd8c1057fe401057f200ac8ea5fcb1f2f9b
Noah Davis <davisprints3d@gmail.com>
2024-03-09 20:20:19 -0600

    Implimented snap to position and loading  images from stripe api

--- coffee_payment.py
+++ coffee_payment.py
@@ -1,13 +1,52 @@
 import stripe
 import qrcode
 import time
-
-qrcode.make
+import json
 
 class Stripe():
-    def __init__(self, api_key:str) -> None:
+    def __init__(self, apiKey:str) -> None:
         '''
         Parameters:
             api_key - Your stripe api key, https://dashboard.stripe.com/apikeys
         '''
-        stripe.api_key = api_key
+        stripe.api_key = apiKey
+
+    def getProducts(self, numSlots: int) -> list:
+        '''
+        Returns a list of products pulled from stripe.
+        All products must have the metadata of "vending_slot" to be set to an int.
+        If the specified vending slot is not found, a new placholder product will be created.
+
+        Parameters:
+            numSlots - The number of products to be fetched to populate your machines kpod slots
+        '''
+        
+        products = []
+
+        # populate machines vending slots with items
+        for slot in range(numSlots):
+
+            # cycle through products until a slot canidate is found
+            for product in stripe.Product.list().data:
+                if "vending_slot" in product.metadata.keys() and product.metadata["vending_slot"] == str(slot):
+                    products.append(product)
+                    break
+
+            # slot was not filled, populate with a new product
+            if (len(products) - 1) < slot:
+                product = stripe.Product.create(name=f"Slot {slot} Placeholder",
+                                                active=False,
+                                                metadata={"vending_slot":slot})
+                products.append(product)
+                
+        return products
+
+if __name__ == '__main__':
+
+    with open("config.json", "r") as config_file:  # Open in read mode
+        api_key = json.load(config_file)["stripe"]["api_key"]  # Load data from the opened file
+
+    payment_handler = Stripe(api_key)
+
+    for product in payment_handler.getProducts(5):
+        print(product.name)
--- main.py
+++ main.py
@@ -2,14 +2,18 @@ import subprocess
 import sys
 import os
 import time
+import urllib.request
+import json
+
+from PyQt6.QtGui import QPaintEvent, QResizeEvent
 subprocess.run(["pip", "install", "-r", "requirements.txt"])
 
-import coffee_payment
 from PyQt6.QtCore import *
 from PyQt6.QtGui import *
 from PyQt6.QtWidgets import *
 from PyQt6.QtWidgets import *
 from PIL.ImageQt import ImageQt
+from coffee_payment import Stripe
 
 ##Example code for loading qr code supplied by coffee_payment.Stripe()
 #qim = ImageQt(your_qr_code)
@@ -19,6 +23,8 @@ class stackedExample(QWidget):
 
     def __init__(self):
         super(stackedExample, self).__init__()
+
+        self.payment_handler = Stripe(self.getConfig()["stripe"]["api_key"])
             
         self.stack1 = QWidget()
         self.stack2 = QWidget()
@@ -34,7 +40,15 @@ class stackedExample(QWidget):
         self.setWindowTitle('StackedWidget demo')
         self.showFullScreen()
         self.show()
-		
+
+    def getConfig(self) -> dict:
+
+        "Returns the dictionary stored in config.json"
+        with open("config.json", "r") as config_file:  # Open in read mode
+            config = json.load(config_file)  # Load data from the opened file
+
+        return config
+        
     def stack1UI(self):
  
         layout = QVBoxLayout()
@@ -49,7 +63,7 @@ class stackedExample(QWidget):
         layout.setAlignment(Qt.AlignmentFlag.AlignAbsolute)
 
         self.stack1.setLayout(layout)
-		
+        
     def stack2UI(self):
         layout = QHBoxLayout()
         layout.setSpacing(0)
@@ -63,7 +77,7 @@ class stackedExample(QWidget):
         sidebar_layout.addStretch()
 
         pay_qr_image = QLabel()
-        pay_qr_image.setPixmap(QPixmap('placeholder.png').scaledToHeight(400))
+        pay_qr_image.setPixmap(QPixmap('placeholder.jpg').scaledToHeight(400))
         pay_qr_image.hide()
         sidebar_layout.addWidget(pay_qr_image)
         sidebar_layout.addStretch()
@@ -108,7 +122,8 @@ class stackedExample(QWidget):
                                     }
                                     ''')
 
-        product_slider = DraggableScrollArea()
+        product_slider = ProductSelection()
+        product_slider.setProducts(self.payment_handler.getProducts(self.getConfig()["hardware"]["vending_slots"]))
         # product_slider.setHorizontalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOff)
         # product_slider.setVerticalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOff)
         product_slider.setAlignment(Qt.AlignmentFlag.AlignCenter)
@@ -204,27 +219,6 @@ class stackedExample(QWidget):
             self.timer.start(5)
         
         pay_button.clicked.connect(lambda *_: set_payment_view(show=True))
-        
-        product_layout = QHBoxLayout()
-        product_layout.setSpacing(500)
-        product_layout.setContentsMargins(500, 0, 500, 0)
-
-        product_slider.eventFilter
-        products = []
-        for index in range(5):
-            products.append(QLabel())
-            products[index].setPixmap(QPixmap('placeholder.png').scaledToHeight(500))
-
-            product_layout.addWidget(products[index])
-
-        button = QPushButton("test")
-        button.clicked.connect(lambda *_: product_slider.ensureWidgetVisible(products[3], 1000, 1000))
-        sidebar_layout.addWidget(button)
-
-        scroll_widget = QWidget()
-        scroll_widget.setLayout(product_layout)
-
-        product_slider.setWidget(scroll_widget)
 
         layout.addWidget(product_slider)
         layout.addWidget(sidebar_widget)
@@ -239,32 +233,78 @@ class stackedExample(QWidget):
         if self.Stack.currentWidget() == self.stack1:
             self.Stack.setCurrentWidget(self.stack2)
 
-class DraggableScrollArea(QScrollArea):
-    def __init__(self, *args, **kwargs):
+class ProductSelection(QScrollArea):
+    def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
         self.setWidgetResizable(True)  # Allow resizing of scroll area content
         self.last_drag_pos = None
+
+        self.product_image_spacing = 100
+        self.product_image_size = 500
+        self.product_layout = QHBoxLayout()
+        self.product_layout.setSpacing(self.product_image_spacing)
+
+        self.scroll_widget = QWidget()
+        self.scroll_widget.setLayout(self.product_layout)
+
+        self.setWidget(self.scroll_widget)
+
+    def getProductName(self) -> str:
+        return self.products[self.current_product_index].name
+
+    def setProducts(self, products: list[Stripe]) -> None:
+        self.products = products
+        self.product_widgets = []
+
+        for index in products:
+
+            new_widget = QLabel()
+
+            # set to placeholder if image isn't found
+            if len(index.images) == 0:
+                pixmap = QPixmap('placeholder.jpg')
+            
+            else:
+                # load image from url specified on stripe product page
+                with urllib.request.urlopen(index.images[0]) as url:
+                    data = url.read()
+
+                pixmap = QPixmap()
+                pixmap.loadFromData(data)
+
+            new_widget.setPixmap(pixmap.scaled(self.product_image_size, self.product_image_size))
+
+            self.product_layout.addWidget(new_widget)
+            self.product_widgets.append(new_widget)
 
     def mousePressEvent(self, event):
         if event.buttons() == Qt.MouseButton.LeftButton:
             self.last_drag_pos = event.pos()
 
-    def mouseMoveEvent(self, event):
+    def mouseMoveEvent(self, event) -> None:
         if self.last_drag_pos is not None:
             delta = event.pos() - self.last_drag_pos
             self.horizontalScrollBar().setValue(self.horizontalScrollBar().value() - delta.x())
             self.verticalScrollBar().setValue(self.verticalScrollBar().value() - delta.y())
             self.last_drag_pos = event.pos()
 
-    def mouseReleaseEvent(self, event):
+    def resizeEvent(self, a0: QResizeEvent | None) -> None:
+        # set content margins so that products can be scrolled completely out of view
+        self.product_layout.setContentsMargins(self.width(), 0, self.width(), 0)
+
+        # center on first product
+        self.mouseReleaseEvent()
+
+    def mouseReleaseEvent(self, *kwargs) -> None:
         """Centers the slider on the closest widget when the user releases it."""
 
         # Get slider position and widget size
         slider_pos = self.horizontalScrollBar().sliderPosition()
-        widget_spacing = (self.widget().width() - (self.width() / 2)) / 5  # Assuming six equal-width widgets
+
+        widget_spacing = self.product_image_size + self.product_image_spacing
 
         # Calculate potential snap positions (centers of each widget)
-        snap_positions = [i * (widget_spacing / 2) for i in range(5)]
+        snap_positions = [(i * widget_spacing) + ((self.width() + self.product_image_size) / 2) for i in range(len(self.products))]
 
         # Find the closest snap position based on current slider position
         closest_snap_pos = min(snap_positions, key=lambda pos: abs(pos - slider_pos))
@@ -275,10 +315,26 @@ class DraggableScrollArea(QScrollArea):
         # Clear last drag position (optional for potential future use)
         self.last_drag_pos = None
 
+        # set grayed out item images
+        self.current_product_index = snap_positions.index(closest_snap_pos)
+        for index in range(len(self.products)):
+
+            opacity_effect = QGraphicsOpacityEffect()
+
+            if index == self.current_product_index:
+                # Create the opacity effect
+                opacity_effect.setOpacity(1.0)  # Set desired opacity (0.0 - fully transparent, 1.0 - opaque)
+
+            else:
+                opacity_effect.setOpacity(0.3)
+
+            # Apply the effect to the widget
+            self.product_widgets[index].setGraphicsEffect(opacity_effect)
+
 def main():
     app = QApplication(sys.argv)
     ex = stackedExample()
     sys.exit(app.exec())
-	
+    
 if __name__ == '__main__':
     main()
